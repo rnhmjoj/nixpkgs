e7e4dd670a2236a01adc42599f157e710f65de70
rnhmjoj <rnhmjoj@inventati.org>
2021-02-02 02:47:36 +0100

    thinkfan: cleanup
    - Remove the custom installPhase: upstream has added an install target
    and it does a better job than we do
    
    - Improve meta: description -> longDescription and add a short one
    
    - Fix license: GPL3 -> GPL3+
    
    (cherry picked from commit 55b97f7ce62eb93e353b1d8af332f4c9323b5f8f)

--- pkgs/tools/system/thinkfan/default.nix
+++ pkgs/tools/system/thinkfan/default.nix
@@ -1,5 +1,13 @@
-{ stdenv, fetchFromGitHub, cmake, libyamlcpp, pkgconfig
-, smartSupport ? false, libatasmart }:
+{ lib
+, stdenv
+, fetchFromGitHub
+, cmake
+, libyamlcpp
+, pkg-config
+, procps
+, coreutils
+, smartSupport ? false, libatasmart
+}:
 
 stdenv.mkDerivation rec {
   pname = "thinkfan";
@@ -15,32 +23,22 @@ stdenv.mkDerivation rec {
   cmakeFlags = [
     "-DCMAKE_INSTALL_DOCDIR=share/doc/${pname}"
     "-DUSE_NVML=OFF"
-  ] ++ stdenv.lib.optional smartSupport "-DUSE_ATASMART=ON";
+  ] ++ lib.optional smartSupport "-DUSE_ATASMART=ON";
 
-  nativeBuildInputs = [ cmake pkgconfig ];
+  nativeBuildInputs = [ cmake pkg-config ];
 
-  buildInputs = [ libyamlcpp ] ++ stdenv.lib.optional smartSupport libatasmart;
+  buildInputs = [ libyamlcpp ] ++ lib.optional smartSupport libatasmart;
 
-  installPhase = ''
-    runHook preInstall
-
-    install -Dm755 {.,$out/bin}/thinkfan
-
-    cd "$NIX_BUILD_TOP"; cd "$sourceRoot" # attempt to be a bit robust
-    install -Dm644 {.,$out/share/doc/thinkfan}/README.md
-    cp -R examples $out/share/doc/thinkfan
-    install -Dm644 {src,$out/share/man/man1}/thinkfan.1
-
-    runHook postInstall
-  '';
-
-  meta = with stdenv.lib; {
-    description = "A minimalist fan control program. Originally designed
-specifically for IBM/Lenovo Thinkpads, it now supports any kind of system via
-the sysfs hwmon interface (/sys/class/hwmon).";
-    license = licenses.gpl3;
+  meta = {
+    description = "A simple, lightweight fan control program";
+    longDescription = ''
+      Thinkfan is a minimalist fan control program. Originally designed
+      specifically for IBM/Lenovo Thinkpads, it now supports any kind of
+      system via the sysfs hwmon interface (/sys/class/hwmon).
+    '';
+    license = lib.licenses.gpl3Plus;
     homepage = "https://github.com/vmatare/thinkfan";
-    maintainers = with maintainers; [ domenkozar ];
-    platforms = platforms.linux;
+    maintainers = with lib.maintainers; [ domenkozar ];
+    platforms = lib.platforms.linux;
   };
 }
